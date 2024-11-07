495d0beec7eff4f027a47028a13f7fa83375213c
KimDohYon <88271006+KimDohYon@users.noreply.github.com>
2024-11-07 15:22:54 +0900

    Update install.sh

--- PiTextReader-master/install.sh
+++ PiTextReader-master/install.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
-# Install PiTextReader
+# Install PiTextReader with PaddleOCR
 # 
 # Run using:
 # $ sh install.sh
 #
 # Can be safely run multiple times
 #
-# version 20180210
+# version 20231107
 #
 
 # Make sure python requirements are installed
 sudo apt-get update
-#sudo apt-get install -y build-essential python-pip python-dev 
 
-echo  
+# Install necessary packages
+# flite는 TTS(Text-to-Speech)용 패키지입니다.
+sudo apt-get install -y flite
 
-# Install packages
-sudo apt-get install -y tesseract-ocr flite
+# Install Python and pip if not already installed
+# 파이썬과 pip 설치 (라즈베리파이에 파이썬3가 기본 설치되어 있지 않은 경우)
+sudo apt-get install -y python3 python3-pip
 
+# Install PaddleOCR and PaddlePaddle
+echo "Installing PaddleOCR and PaddlePaddle..."
+pip3 install paddlepaddle
+pip3 install paddleocr
 
 # Verify Camera is configured
 X=`raspistill -o test.jpg 2>&1|grep Failed`
 
 if [ -z "$X" ];
 then
-        echo "Found Camera OK"
+    echo "Found Camera OK"
 else
-	echo $X
-        echo "NO Camera Detected! SEE DOCS Troubleshooting section."
-	exit
+    echo $X
+    echo "NO Camera Detected! SEE DOCS Troubleshooting section."
+    exit
 fi 
 
-# Install custom software
+# Install custom software (crontab 설정)
 crontab ./cronfile
 echo "Crontab entry installed for pi userid. OK"
- 
+
 # FINISHED!
 echo "Finished installation. See Readme.md for more info"
-echo "Reboot your pi now:  $ sudo reboot"
-echo 
+echo "Reboot your pi now: $ sudo reboot"
 
